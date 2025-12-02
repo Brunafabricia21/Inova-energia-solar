a191c8ea03a86d659b189a9ddbd5d37289e67b11
Brunafabricia21 <brunafabriciadesousa@gmail.com>
2025-12-02 17:47:10 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,28 +3,9 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-<<<<<<< HEAD
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-=======
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/Inova-energia-solar/', // <== essencial pro GH Pages
+    base: '/Inova-energia-solar/', // <== correto pro GH Pages
     server: {
       port: 3000,
       host: '0.0.0.0',
@@ -40,5 +21,4 @@ export default defineConfig(({ mode }) => {
       }
     }
   };
->>>>>>> origin/main
 });
